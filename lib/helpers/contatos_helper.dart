aed890cbf3a27c0c20b5afc3b484eb5d658973e7
PeterCcT <pedrorodriguesfereira@gmail.com>
2020-04-20 17:19:39 -0300

    Adicionado a classe HelperContato método para criação de banco de dados

--- lib/helpers/contatos_helper.dart
+++ lib/helpers/contatos_helper.dart
@@ -1,7 +1,10 @@
+import 'package:path/path.dart';
+import 'dart:async';
 import 'package:flutter/rendering.dart';
 import 'package:sqflite/sqflite.dart';
 
 //colunas do database
+final nomeTabela = "contatos";
 final idBd = "idCol";
 final nomeBd = "nomeCol";
 final emailBd = "emailCol";
@@ -9,7 +12,37 @@ final telBd = "telefoneCol";
 final imgBd = "imgCol";
 
 class HelperContato {
-  //atributos
+  //singleton
+  static final HelperContato _instance = HelperContato.internal();
+  factory HelperContato() => _instance;
+  HelperContato.internal();
+
+  Database _db;
+
+  Future<Database>get db async {
+    if (_db != null) {
+      return _db;
+    } else {
+      _db = await initDb();
+      return _db;
+    }
+  }
+
+  //inicializando banco de dados
+  Future<Database> initDb() async {
+    final localBd = await getDatabasesPath();
+    final caminhoBd = join(localBd, "contatos.db");
+
+    return await openDatabase(
+      caminhoBd,
+      version: 1,
+      onCreate: (Database db, int vRecente) async {
+        await db
+            .execute("create table $nomeBd($idBd int primary key,$nomeBd text"
+                ",$emailBd text,$telBd text, $imgBd text)");
+      },
+    );
+  }
 }
 
 class Contato {
